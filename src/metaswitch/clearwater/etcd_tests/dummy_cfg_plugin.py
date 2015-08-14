93fcc56f63bf6259be195606ab6b19fe94d38aa8
Rob Day <Robert.Day@metaswitch.com>
2015-08-14 16:59:41 +0000

    Add automatic cluster manager FV tests

--- src/metaswitch/clearwater/etcd_tests/dummy_cfg_plugin.py
+++ src/metaswitch/clearwater/etcd_tests/dummy_cfg_plugin.py
@@ -1,6 +1,39 @@
 from metaswitch.clearwater.config_manager import plugin_base
+from metaswitch.clearwater.cluster_manager.plugin_base import SynchroniserPluginBase
 
 from threading import Condition
+from time import sleep
+
+class DummyClusterPlugin(SynchroniserPluginBase):
+    def __init__(self, fn):
+        self.value = None
+        self.fn = fn
+
+    def key(self):
+        return "/dummy/"
+
+    def on_cluster_changing(self, cluster_view):
+        self.value = cluster_view
+
+    def on_joining_cluster(self, cluster_view):
+        self.on_cluster_changing(cluster_view)
+
+    def on_new_cluster_config_ready(self, cluster_view):
+        if self.fn:
+            self.fn()
+            self.fn = None
+        self.on_cluster_changing(cluster_view)
+
+    def on_stable_cluster(self, cluster_view):
+        self.on_cluster_changing(cluster_view)
+
+    def on_leaving_cluster(self, cluster_view):
+        self.on_cluster_changing(cluster_view)
+
+    def wait_for(self, value, timeout=300):
+        for i in range(timeout):
+            if self.value != value:
+                sleep(1)
 
 class DummyCfgPlugin(plugin_base.ConfigPluginBase):
     def __init__(self):
--- src/metaswitch/clearwater/etcd_tests/etcdtest.py
+++ src/metaswitch/clearwater/etcd_tests/etcdtest.py
@@ -1,12 +1,14 @@
-from metaswitch.clearwater.config_manager.etcd_synchronizer import EtcdSynchronizer
+from metaswitch.clearwater.config_manager.etcd_synchronizer import EtcdSynchronizer as CfgEtcdSynchronizer
+from metaswitch.clearwater.cluster_manager.etcd_synchronizer import EtcdSynchronizer as ClusterEtcdSynchronizer
 from time import sleep
 import logging
 import sys
 import etcd
 from threading import Thread
 import unittest
+import json
 
-from dummy_cfg_plugin import DummyCfgPlugin
+from dummy_cfg_plugin import DummyCfgPlugin, DummyClusterPlugin
 from etcdserver import EtcdServer
 
 logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
@@ -15,10 +17,8 @@ logging.getLogger().setLevel(logging.ERROR)
 class TestConfigManager(object):
     def __init__(self, etcd_ip):
         self.plugin = DummyCfgPlugin()
-        self.s = EtcdSynchronizer(self.plugin, etcd_ip, "local", None)
-        self.thread = Thread(target=self.s.main)
-        self.thread.daemon = True
-        self.thread.start()
+        self.s = CfgEtcdSynchronizer(self.plugin, etcd_ip, "local", None)
+        self.s.start_thread()
 
     def value(self):
         return self.plugin.value
@@ -27,13 +27,30 @@ class TestConfigManager(object):
         self.plugin.wait_for(*args, **kwargs)
 
     def stop(self):
-        self.s._terminate_flag = True
+        self.s.terminate()
+
+class TestClusterManager(object):
+    def __init__(self, etcd_ip, during_fn):
+        self.plugin = DummyClusterPlugin(during_fn)
+        self.s = ClusterEtcdSynchronizer(self.plugin, etcd_ip, pause_before_retry=2, fsm_delay=1)
+        self.s.start_thread()
+
+    def value(self):
+        return self.plugin.value
+
+    def wait_for(self, *args, **kwargs):
+        self.plugin.wait_for(*args, **kwargs)
+
+    def stop(self):
+        self.s.terminate()
+
 
 
 class EtcdTestBase(unittest.TestCase):
     def __init__(self, x):
         self.servers = {}
         self.config_manager = {}
+        self.cluster_manager = {}
         unittest.TestCase.__init__(self, x)
 
     def add_server(self, ip, **kwargs):
@@ -51,9 +68,16 @@ class EtcdTestBase(unittest.TestCase):
         self.config_manager[etcd_ip] = mgr
         return mgr
 
+    def add_cluster_manager(self, etcd_ip, fn=None):
+        mgr = TestClusterManager(etcd_ip, fn)
+        self.cluster_manager[etcd_ip] = mgr
+        return mgr
+
     def tearDown(self):
         print "Tearing down!"
         for client in self.config_manager.values():
+            client.stop()
+        for client in self.cluster_manager.values():
             client.stop()
         for server in self.servers.values():
             server.crash()
@@ -73,6 +97,20 @@ class EtcdTestBase(unittest.TestCase):
     def restart_remote_server(self):
         self.servers['127.0.0.102'].crash()
         self.add_server('127.0.0.102', existing='127.0.0.100', replacement=True)
+
+    def restart_quorum(self):
+        self.servers['127.0.0.101'].crash()
+        self.servers['127.0.0.102'].crash()
+        self.delay_one_second(self.add_server, '127.0.0.101', existing='127.0.0.100', replacement=True)
+        self.delay_one_second(self.add_server, '127.0.0.102', existing='127.0.0.100', replacement=True)
+
+    def add_five_nodes(self):
+        for ip in ['127.0.0.200',
+                   '127.0.0.201',
+                   '127.0.0.202',
+                   '127.0.0.203',
+                   '127.0.0.204']:
+            self.add_server(ip, existing='127.0.0.100')
 
     def stop_remote_server(self):
         self.servers['127.0.0.102'].crash()
@@ -100,6 +138,32 @@ class EtcdTestBase(unittest.TestCase):
         mgr.wait_for('hello world 2', 35)
         self.assertEqual(mgr.value(), "hello world 2")
 
+    def cluster_mgr_test(self, before_fn, during_fn):
+        self.initialise_servers('127.0.0.100', '127.0.0.101', '127.0.0.102')
+        client = self.servers['127.0.0.100'].client()
+        client.write("/dummy/", "{}")
+
+        if before_fn:
+            before_fn()
+
+        mgr1 = self.add_cluster_manager('127.0.0.100')
+
+        mgr2 = self.add_cluster_manager('127.0.0.101', during_fn)
+        mgr3 = self.add_cluster_manager('127.0.0.102')
+
+        expected = {'127.0.0.100': 'normal',
+                    '127.0.0.101': 'normal',
+                    '127.0.0.102': 'normal'}
+
+        mgr1.wait_for(expected)
+        mgr2.wait_for(expected)
+
+        self.assertEqual(mgr1.value(), expected)
+        self.assertEqual(mgr2.value(), expected)
+
+        # Don't assert anything about mgr3 - we may have killed that server
+
+
     def test_one(self):
         self.config_mgr_test(before_fn=None, during_fn=None)
 
@@ -111,3 +175,21 @@ class EtcdTestBase(unittest.TestCase):
 
     def test_four(self):
         self.config_mgr_test(before_fn=None, during_fn=self.stop_remote_server)
+
+    def test_four_a(self):
+        self.config_mgr_test(before_fn=None, during_fn=self.add_five_nodes)
+
+    def test_four_b(self):
+        self.config_mgr_test(before_fn=None, during_fn=self.restart_quorum)
+
+    def test_five(self):
+        self.cluster_mgr_test(before_fn=None, during_fn=None)
+
+    def test_six(self):
+        self.cluster_mgr_test(before_fn=None, during_fn=self.restart_local_server)
+
+    def test_seven(self):
+        self.cluster_mgr_test(before_fn=None, during_fn=self.restart_quorum)
+
+    def test_six(self):
+        self.cluster_mgr_test(before_fn=None, during_fn=self.add_five_nodes)
