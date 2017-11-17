6b133006c653a00c913659dc112c8813e63f8135
Michael <michael.duppre@metaswitch.com>
2017-11-17 15:12:29 +0000

    Code review markups

--- src/metaswitch/clearwater/config_manager/print_diff_and_syslog_entrypoint.py
+++ src/metaswitch/clearwater/config_manager/print_diff_and_syslog_entrypoint.py
@@ -6,15 +6,15 @@ from metaswitch.clearwater.config_manager.config_access import print_diff_and_sy
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config_type', type=str)
-    parser.add_argument('--config_1_path', type=str)
-    parser.add_argument('--config_2_path', type=str)
+    parser.add_argument('--config_before', type=str)
+    parser.add_argument('--config_after', type=str)
     args = parser.parse_args()
 
-    with open(args.config_1_path, 'r') as config_1_file:
-        config_1 = config_1_file.read()
+    with open(args.config_before, 'r') as config_before_file:
+        config_1 = config_before_file.read()
 
-    with open(args.config_2_path, 'r') as config_2_file:
-        config_2 = config_2_file.read()
+    with open(args.config_after, 'r') as config_after_file:
+        config_2 = config_after_file.read()
 
     print_diff_and_syslog(args.config_type, config_1, config_2)
 
