220116452333531e4bbd0ce7a02236c472c054a6
Michael Duppre <michael.duppre@metaswitch.com>
2018-04-06 17:39:31 +0100

    Fix UTs

--- src/metaswitch/clearwater/queue_manager/test/test_timers.py
+++ src/metaswitch/clearwater/queue_manager/test/test_timers.py
@@ -9,12 +9,12 @@
 
 from metaswitch.clearwater.etcd_shared.test.mock_python_etcd import EtcdFactory
 from metaswitch.clearwater.queue_manager.etcd_synchronizer import EtcdSynchronizer
-from metaswitch.clearwater.queue_manager.timers import QueueTimer
 from .plugin import TestNoTimerDelayPlugin
 from mock import patch
 from .test_base import BaseQueueTest
 
 alarms_patch = patch("metaswitch.clearwater.queue_manager.alarms.alarm_manager")
+
 
 class TimersTest(BaseQueueTest):
     @patch("etcd.Client", new=EtcdFactory)
@@ -26,8 +26,10 @@ class TimersTest(BaseQueueTest):
     @patch("metaswitch.clearwater.queue_manager.timers.QueueTimer")
     def test_no_timer_set_when_processing(self, mock_queue_timer):
         """Test that when a node is in processing state, it doesn't set a timer for itself."""
-        # Write some initial data into the key
-        self.set_initial_val("{\"FORCE\": false, \"ERRORED\": [], \"COMPLETED\": [], \"QUEUED\": [{\"ID\":\"10.0.0.1-node\",\"STATUS\":\"PROCESSING\"}]}")
+        self.set_initial_val("{\"FORCE\": false,"
+                             " \"ERRORED\": [],"
+                             " \"COMPLETED\": [],"
+                             " \"QUEUED\": [{\"ID\":\"10.0.0.1-node\",\"STATUS\":\"PROCESSING\"}]}")
 
         # As it is only one node, it keeps in the processing state as the plugin doesn't remove it
         # from the queue
@@ -44,7 +46,10 @@ class TimersTest(BaseQueueTest):
 
     def test_other_node_timer_pop(self):
         """Test that when a timer pops for another node it marks the other node as failed."""
-        self.set_initial_val("{\"FORCE\": false, \"ERRORED\": [], \"COMPLETED\": [], \"QUEUED\": [{\"ID\":\"10.0.0.2-node\",\"STATUS\":\"PROCESSING\"}]}")
+        self.set_initial_val("{\"FORCE\": false,"
+                             " \"ERRORED\": [],"
+                             " \"COMPLETED\": [],"
+                             " \"QUEUED\": [{\"ID\":\"10.0.0.2-node\",\"STATUS\":\"PROCESSING\"}]}")
 
         def pass_criteria(val):
             return (1 == len(val.get("ERRORED"))) and \
@@ -55,33 +60,36 @@ class TimersTest(BaseQueueTest):
 
         self.assertTrue(self.wait_for_success_or_fail(pass_criteria))
 
-    def test_timer_pop_force(self):
-        """Test that when a timer pops when force is true it doesn't clear the queue"""
-        self.set_initial_val("{\"FORCE\": true, \"ERRORED\": [], \"COMPLETED\": [], \"QUEUED\": [{\"ID\":\"10.0.0.1-node\",\"STATUS\":\"PROCESSING\"},{\"ID\":\"10.0.0.2-node\",\"STATUS\":\"QUEUED\"}]}")
-
-        self._e._fsm._timer = QueueTimer(self._e._fsm._timer_callback_func)
-        self._e._fsm._timer.timer_popped = True
+    def test_timer_pop_force_true(self):
+        """Test that when a timer pops for another node with force=true it doesn't clear the queue."""
+        self.set_initial_val("{\"FORCE\": true,"
+                             " \"ERRORED\": [],"
+                             " \"COMPLETED\": [],"
+                             " \"QUEUED\": [{\"ID\":\"10.0.0.2-node\",\"STATUS\":\"PROCESSING\"},{\"ID\":\"10.0.0.1-node\",\"STATUS\":\"QUEUED\"}]}")
 
         def pass_criteria(val):
-            return (2 == len(val.get("ERRORED"))) and \
+            return (1 == len(val.get("ERRORED"))) and \
                    (0 == len(val.get("COMPLETED"))) and \
-                   (0 == len(val.get("QUEUED"))) and \
-                   ("10.0.0.1-node" == val.get("ERRORED")[0]["ID"]) and \
+                   (1 == len(val.get("QUEUED"))) and \
+                   ("10.0.0.2-node" == val.get("ERRORED")[0]["ID"]) and \
                    ("UNRESPONSIVE" == val.get("ERRORED")[0]["STATUS"]) and \
-                   ("10.0.0.2-node" == val.get("ERRORED")[1]["ID"]) and \
-                   ("UNRESPONSIVE" == val.get("ERRORED")[1]["STATUS"])
+                   ("10.0.0.1-node" == val.get("QUEUED")[0]["ID"]) and \
+                   ("PROCESSING" == val.get("QUEUED")[0]["STATUS"])
 
         self.assertTrue(self.wait_for_success_or_fail(pass_criteria))
 
-    def test_timer_pop_no_force(self):
-        """Test that when a timer pops when force is false it does clear the queue"""
-        self.set_initial_val("{\"FORCE\": false, \"ERRORED\": [], \"COMPLETED\": [], \"QUEUED\": [{\"ID\":\"10.0.0.1-node\",\"STATUS\":\"PROCESSING\"},{\"ID\":\"10.0.0.2-node\",\"STATUS\":\"PROCESSING\"}]}")
+    def test_timer_pop_force_false(self):
+        """Test that when a timer pops for another node with force=false it does clear the queue."""
+        self.set_initial_val("{\"FORCE\": false,"
+                             " \"ERRORED\": [],"
+                             " \"COMPLETED\": [],"
+                             " \"QUEUED\": [{\"ID\":\"10.0.0.2-node\",\"STATUS\":\"PROCESSING\"},{\"ID\":\"10.0.0.1-node\",\"STATUS\":\"QUEUED\"}]}")
 
         def pass_criteria(val):
             return (1 == len(val.get("ERRORED"))) and \
                    (0 == len(val.get("COMPLETED"))) and \
                    (0 == len(val.get("QUEUED"))) and \
-                   ("10.0.0.1-node" == val.get("ERRORED")[0]["ID"]) and \
+                   ("10.0.0.2-node" == val.get("ERRORED")[0]["ID"]) and \
                    ("UNRESPONSIVE" == val.get("ERRORED")[0]["STATUS"])
 
         self.assertTrue(self.wait_for_success_or_fail(pass_criteria))
